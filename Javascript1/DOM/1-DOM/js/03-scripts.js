3fb385de40d68ef518312b80ddb411f7f0bcba61
Arlex25 <108701587+Arlex25@users.noreply.github.com>
2022-11-23 20:34:59 -0500

    Cambio Icono de pagina
    Cambio icono de la pagina e input de busqueda

--- Javascript1/DOM/1-DOM/js/03-scripts.js
+++ Javascript1/DOM/1-DOM/js/03-scripts.js
@@ -1,17 +1,27 @@
-const encabezado = document.querySelector('.contenido-hero h1').textContent;
-console.log(encabezado);
+// Titulo de la pagina
+document.title = 'BARBER SENA SHOP';
 
-const tituloPagina = 'BARBER SENA SHOP';
-document.title = tituloPagina;
-
+// Cambio fondo de la pagina
 const nuevoFondo = document.querySelector('.hero');
 nuevoFondo.style.backgroundImage = 'url(img/barber/barbershop.jpg)';
 
+// Cambio logo de la pagina
 const nuevoLogo = document.querySelector('.logo img');
 nuevoLogo.src = 'img/barber/Logo.png';
 
+
+// Cambio encabezado de la pagina, input tipo text y su placeholder
 const nuevoencabezado = 'BARBER SENA SHOP';
 document.querySelector('.contenido-hero h1').textContent = nuevoencabezado;
+document.querySelector('.contenido-hero h1').style.textAlign= 'center';
+
+const barraBusqueda = document.querySelector('.busqueda');
+barraBusqueda.placeholder = 'Cortes, Barbas, Tintes';
+
+const inputBusqueda = document.querySelector('.contenido-hero form');
+console.log(inputBusqueda);
+inputBusqueda.style.gridColumn = '1 / 4';
+
 
 // const imagen = document.querySelector('.card img');
 // imagen.src = 'img/hacer4.jpg';
